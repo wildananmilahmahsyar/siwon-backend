5f793c25cd7987397a8cdcfb12035f0ac5e6bd2e
wildananmilahmahsyar <wildananmillahmashyar@gmail.com>
2025-06-10 23:34:14 +0800

    edit db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,16 @@
-// config/db.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('siwon', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: false, // matikan jika tidak ingin log SQL
+  }
+);
 
 module.exports = sequelize;
