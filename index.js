2d28ec384e7cf5889455e1bec4fdf6a7441e4018
wildananmilahmahsyar <wildananmillahmashyar@gmail.com>
2025-06-11 05:51:17 +0800

    fix: manual CORS & user route debug

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const uploadRoutes = require('./src/routes/uploads');
 const path = require('path');
 
 const app = express();
+router.options('/', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://siwon-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  return res.sendStatus(200);
+});
 
 // ✅ CORS MANUAL FIX (untuk semua jenis request)
 app.use((req, res, next) => {
