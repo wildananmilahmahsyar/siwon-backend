896d5d9c39faaf8ec02426be70bcbbda01e25fc9
wildananmilahmahsyar <wildananmillahmashyar@gmail.com>
2025-06-11 22:28:32 +0800

    tesdb

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,9 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: false,
+});
 
 module.exports = sequelize;
